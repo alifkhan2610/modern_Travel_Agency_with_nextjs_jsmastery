7961fe66b1e2ed0a615f7b9465e6d90844d5f45b
MD ALIF <alifkhan.bubt@gmail.com>
2024-02-12 17:50:43 +0600

    navber,hero section is done and camp setion is ongoing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,10 +4,10 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className="max-container border-2 padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
       <div className="hero-map" />
-      {/* LEFT^ */}
 
+      {/* LEFT SIDE */}
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image
           src="/camp.svg"
@@ -18,46 +18,65 @@ const Hero = () => {
         />
         <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, veniam
-          quidem doloremque tenetur quasi enim tempore, dolorum totam molestiae
-          et voluptatem. Laborum quo illo autem alias magni atque officia
-          veniam.
+          We want to be on each of your journeys seeking the satisfaction of
+          seeing the incorruptible beauty of nature. We can help you on an
+          adventure around the world in just one app
         </p>
-      </div>
-      <div className="my-11 flex flex-wrap gap-5">
-        <div className="flex flex-center gap-2">
-          {Array(5)
-            .fill(1)
-            .map((_, index) => (
-              <Image
-                src="/star.svg"
-                alt="star"
-                key={index}
-                height={24}
-                width={24}
-              />
-            ))}
+
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex flex-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  src="/star.svg"
+                  alt="star"
+                  key={index}
+                  height={24}
+                  width={24}
+                />
+              ))}
+          </div>
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            198k
+            <span className="regular-16 lg:regular-20 ml-1">
+              Excelent Review
+            </span>
+          </p>
         </div>
-        <p className="bold-16 lg:bold-20 text-blue-70">
-          198k
-          <span className="regular-16 lg:regular-20 ml-1">Excelent Review</span>
-        </p>
+        <div className="flex w-full flex-col gap-3 sm:flex-row">
+          <Button type="button" title="Download App" variant="btn_green" />
+
+          <Button
+            type="button"
+            title="How we work?"
+            icon="play.svg"
+            variant="btn_white_text"
+          />
+        </div>
       </div>
 
-      <div className="flex w-full flex-col gap-3 sm:flex-row">
-        <Button 
-        type="button" 
-        title="Download App" 
-        variant="btn_green" />
-
-        <Button
-        type="button" 
-        title="How we work?"
-        icon="play.svg" 
-        variant="btn_white_text"
-        />
+      <div className="relative flex flex-1 items-start  ">
+        <div className="relative z-20 w-[268px] flex flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+          <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" height={24} width={24} />
+            </div>
+            <p className="bold-20 text-white">Aguas Calintase</p>
+          </div>
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Distance</p>
+              <p className="bold-20 text-white">173.28 mi</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Elevation</p>
+              <p className="bold-20 text-white">2.040 km</p>
+            </div>
+          </div>
+        </div>
       </div>
-      
     </section>
   );
 };
